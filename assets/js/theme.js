6476d68a319a0dc3008846c0cf3214570cc34117
Alma Kristiāna Vītola <69206400+AKVitola@users.noreply.github.com>
2020-10-26 15:26:29 +0200

    first commit

---
+++ assets/js/theme.js
@@ -0,0 +1,142 @@
+        //====== Play video in an overlay ======
+        //============================
+
+const videoImg = document.getElementById("js-video-img");
+const video = document.getElementById("js-video");
+const pageOverlay = document.getElementById("js-ovelay");
+const pageBcground = document.getElementById("js-video-bcground");
+
+videoImg.addEventListener("click", function() {
+  showVideo();
+});
+
+pageBcground.addEventListener("click", function() {
+  hideVideo();
+});
+
+function showVideo() {
+  pageOverlay.style.display = "block";
+  pageBcground.style.display = "block";
+  video.style.display = "block";
+  playVidDelay(1000);
+  video.currentTime = 0;
+  stopPageScrolling();
+  hideButtonInOverlayMenu();
+
+}
+
+function hideVideo() {
+  video.style.display = "none";
+  pageOverlay.style.display = "none";
+  pageBcground.style.display = "none";
+  video.pause();
+  startPageScrolling();
+  displayGoToTopButton();
+}
+
+function playVidDelay(delay) {
+  setTimeout(function(){
+     video.play();
+  }, delay)
+}
+
+
+        // ====== Overlay menu======
+        //============================
+
+function closeMenu() {
+  document.getElementById("js-nav").style.height = "0%";
+}
+
+function openMenu() {
+  document.getElementById("js-nav").style.height = "100%";
+}
+
+function toggleMenu() {
+  if (document.getElementById("js-nav").style.height !== "100%") {
+    openMenu();
+    stopPageScrolling();
+    hideButtonInOverlayMenu();
+  } else {
+    closeMenu();
+    startPageScrolling();
+    displayGoToTopButton();
+  }
+}
+
+function stopPageScrolling() {
+  document.body.style.overflow = "hidden";
+}
+
+function startPageScrolling() {
+  document.body.style.overflow = "scroll";
+}
+
+function hideButtonInOverlayMenu() {
+  document.getElementById("js-goToTopButton").style.display = "none";
+}
+
+
+// === Close menu when an list item is clicked on
+
+let liElements = document.getElementById("js-nav").getElementsByTagName("li");
+for (let i = 0; i < liElements.length; i++) {
+  liElements[i].addEventListener("click", closeMenuOnSelect);
+}
+
+function closeMenuOnSelect() {
+  if (document.body.offsetWidth <= 1000) {
+    closeMenu();
+    startPageScrolling();
+  }
+}
+
+        //====== Go to top button ======
+        //==============================
+
+
+//Display go to top button when scrolled 20 px
+
+goToTopButton = document.getElementById("js-goToTopButton");
+window.onscroll = function () {
+  displayGoToTopButton();
+};
+
+function displayGoToTopButton() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    goToTopButton.style.display = "block";
+  } else {
+    goToTopButton.style.display = "none";
+  }
+}
+
+//Goes to the first section with id - introduction
+
+function goToTop() {
+  let targetDiv = document.querySelector("#js-introduction");
+  let targetPosition = calculateTargetPos(targetDiv);
+  scrollToLocation(targetPosition);
+}
+
+function scrollToLocation(targetPosition) {
+  window.scrollTo({
+    top: targetPosition,
+    behavior: "smooth",
+  });
+}
+
+// Can be used if the header is fixed. Than offset = header height.
+//When scrolled to position header dosn't cover the section.
+
+function calculateTargetPos(target, offset = 0) {
+  let targetDivTopPos = target.getBoundingClientRect().top;
+  let windowOffset = window.pageYOffset;
+  let position = targetDivTopPos + windowOffset - offset;
+
+  return position;
+}
+
+
+
+
+
